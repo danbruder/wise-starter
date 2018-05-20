982fde504a1400b74a5828f114d5e419554bb539
Tom Pichaud <tompichaud@me.com>
2018-05-20 22:07:20 +0100

    + Add : Favicon and Manifest generator

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,38 @@
 module.exports = {
-  siteMetadata: {
-    title: 'Gatsby Default Starter',
-  },
-  plugins: ['gatsby-plugin-react-helmet'],
+	siteMetadata: {
+		title: 'Gatsby Default Starter'
+	},
+	plugins: [
+		'gatsby-plugin-react-helmet',
+		{
+			resolve: `gatsby-plugin-favicon`,
+			options: {
+				logo: './src/assets/icon.png',
+				injectHTML: true,
+				icons: {
+					android: true,
+					appleIcon: true,
+					appleStartup: true,
+					coast: false,
+					favicons: true,
+					firefox: true,
+					twitter: false,
+					yandex: false,
+					windows: false
+				}
+			}
+		},
+		{
+			resolve: `gatsby-plugin-manifest`,
+			options: {
+				name: 'WiseStarter',
+				short_name: 'WiseStarter',
+				start_url: '/',
+				background_color: '#0E283F',
+				theme_color: '#582644',
+				display: 'minimal-ui',
+				icon: 'src/assets/icon.png'
+			}
+		}
+	]
 }
